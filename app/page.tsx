9da43844c5761b196c7c772b677b4ae09e572c45
Muhammadrasul Rejapov <muhammadrasulrejapov@email.com>
2025-06-03 18:14:38 +0500

    Fix: move main.js to public/static/js and update layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,9 @@
 "use client"
 
-import  from "../static/js/main"
+// import  from "../static/js/main"
+
+import  from "C:\Users\Noutbuk_savdosi\Desktop\TAYYOR PROEKTLAR\wholesale_crm\wholesale_crm\static\js\main.js"
 
 export default function SyntheticV0PageForDeployment() {
   return < />
 }
-// "use client"
-
-// import Head from "next/head"
-
-// export default function SyntheticV0PageForDeployment() {
-//   return (
-//     <>
-//       <Head>
-//         <script src="/static/js/main.js" defer />
-//       </Head>
-//       <div>
-//         <h1>Welcome to CRM System</h1>
-//       </div>
-//     </>
-//   )
-// }
